60be55c8b9abd01d50cca458167af1c89fc197a7
adilraees786 <adilraees911@gmail.com>
2024-05-25 14:29:54 +0500

    Namechange

---
+++ app.js
@@ -0,0 +1,114 @@
+// var str ='  Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque itaque alias incidunt quaerat iusto placeat! Voluptate nemo alias vel provident accusantium sint sunt minima quos praesentium, recusandae est aliquid eos?'
+
+
+
+// var show =document.getElementById('para')
+
+// var defultlength=500
+
+// if(defultlength>=str.length){
+
+//     show.innerHTML=`${str} <button id`
+// }
+//     else{
+
+//     show.innerHTML = str.slice(0,defultlength)
+// }
+
+
+// let hours =0;
+
+// let minut=0;
+
+// let second=0;
+
+// let milisecond=0;
+
+// let timer;
+
+// let stop="stop"
+
+// function start(){
+//     timer = setInterval(()=>{
+//         milisecond++
+//         if(milisecond >=100){
+//             second++
+//             milisecond = 0;
+
+//         }
+//         if(second >=60){
+//             minut++
+//             second = 0;
+
+//         }
+//         if(minut >=60){
+//             hours++
+//             minut=0;
+//         }
+//         document.getElementById('watch').innerHTML = `${hours}:${minut}:${second}:${milisecond}`
+
+
+//     },10)
+// }
+
+// function startstop(){
+//     if( stop == "stop"){
+//         clearInterval(timer)
+// stop =  "stop"
+// document.getElementById("startstop").innerText = "stop";
+
+//     }
+//     else{
+//         start()
+//         document.getElementById("startstop").innerText= "start";
+//     }
+
+// }
+
+// function reset(){
+//     // location.reload()
+
+// hours =0;
+
+//  minut=0;
+
+//  second=0;
+
+// milisecond=0;
+
+
+
+// }
+
+
+
+// var arr= ["ali", "pasha", "zain","haris"]
+//  var index=0;
+
+
+// setInterval(function(){
+//     console.log(arr[index]);
+//     index= [index +1] % arr.length
+// },10000)
+
+// setInterval(()=>{
+
+
+
+// },1000);
+
+
+
+function hello(){
+    console.log("Adil Raees");
+}
+    setInterval(hello,10000)
+
+
+
+
+
+
+
+
+
